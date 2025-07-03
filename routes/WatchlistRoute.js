482a8561f9010af88ed362eebbc523a8b04d47f9
Rakshit <95012114+rakshit1862005@users.noreply.github.com>
2025-07-03 23:34:57 +0530

    Update WatchlistRoute.js

--- routes/WatchlistRoute.js
+++ routes/WatchlistRoute.js
@@ -29,4 +29,51 @@ router.post("/add-to-watchlist", async (req, res) => {
   }
 });
 
+router.post('/remove-from-list',async(req,res)=>{
+  const {email,movie} = req.body;
+  if(!email) return res.status(400).json({message:"Missing Data Email"});
+  if(!movie) return res.status(400).json({message:"Missing Data Movie"});
+
+  try{
+    let userDoc = await WatchlistModel.findOne({email});
+    if(userDoc){
+      userDoc.Watchlist = userDoc.Watchlist.filter(item => item.movie_id !== movie); 
+      await userDoc.save();
+
+    }
+    else{
+      res.status(400).json({message: "Not In List"})
+    }
+    res.status(200).json({ message: "Removed From watchlist" });
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+
+
+});
+
+router.post('/checklist',async(req,res)=>{
+  const {email,movie}=req.body;
+  if(!email) return res.status(400).json({message:"Missing Data Email"});
+  if(!movie) return res.status(400).json({message:"Missing Data Movie"});
+  try{
+    let userDoc = await WatchlistModel.findOne({email});
+    if(userDoc){
+    let status = userDoc.Watchlist.some(item=> item.movie_id==movie);
+      if(status){
+        return res.status(200).json({message:"found"});
+
+      }
+      else{
+        return res.status(200).json({message:"not found"});
+      }
+  }
+}
+catch(error){
+  return res.status(500).json({message:"Something Went Wrong"});
+}
+})
+
 export default router;
